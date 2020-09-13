838d6a114cc64336566d140c157382df4ac55928
adiya99 <40221818+adiya99@users.noreply.github.com>
2020-09-13 22:04:57 +0800

    Update tabel.php

--- tabel.php
+++ tabel.php
@@ -1,170 +1,94 @@
-<br>
-<table id="tb" class="display nowrap" style="width: 100%">
-	<thead>
-		<th>No</th>
-    <?php
-    foreach ($kolom as $key) {
-      echo "<th>".modify($key)."</th>";
-    }
-    ?>		
-		<th>Aksi</th>
-	</thead>
-	<tbody>
-	<?php    	
-      $sql=$db->query($query);
-    	while($row=$sql->fetch_object()) {        
-		$no++;
-    ?>
-		<tr>
-			<td><?php echo $no;?></td>
-        <?php
-    foreach ($baris as $key) {
-      echo "<td>".$row->$key."</td>";
-    }
-    ?>     
-      <td>
-		        <button data-toggle="modal" data-target="#edit<?php echo $row->$pk;?>">
-					<i class="fa fa-edit"></i>
-		        </button>
-		        <button onclick="if(confirm('Are you sure you want to delete your post?')){location.href='konek.php?tabel=<?php echo $tabel;?>&query=hapus&id=<?php echo $row->$pk;?>';return false;};">
-		            <i class="fa fa-trash"></i>
-		        </button>
-      		</td>
-		</tr>
-		<?php }?>
-	</tbody>
-</table>
-
-<script type="text/javascript">  
-  $(document).ready(function() {
-  $('#tb').DataTable({
-  	dom: 'Bfrtip',
-    "scrollX": true,
-  	buttons: [{
-           	text: 'Tambah Data',
-            action: function (e, node, config){
-            		$('#tbh').modal('show')
-                	}
-            },{
-                extend: 'pageLength'
-                            
-            }]
-    });
-});
-</script>
-
-<div class="modal fade" id="tbh">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5>Tambah <?php echo modify("$tabel");?></h5>
-        <button class="close" data-dismiss="modal" ></button>
-      </div>
-      <form action="konek.php?tabel=<?php echo $tabel;?>&query=tambah" method="POST" enctype="multipart/form-data">
-      <div class="modal-body">
-  <?php
-    foreach ($tambahlabel as $key) {
-
-      echo "<label>".modify($key)."</label>";
-
-      if(in_array($key,$typedate)){
-      echo "<input type='date' class='form-control' name='$key'>";
-      }else if(in_array($key,$typefile)){
-      echo "<input type='file' class='form-control' name='$key'>";
-      }else if(@in_array($key,$typefk)){
-        echo "<select class='form-control' name='$key'>";
-        if (in_array($fk,$typefk) && $fk==$key) {
-        echo "<option disabled selected>--- Pilih Data ---</option>";
-        $sql2=$db->query($query2);
-        while($row2=$sql2->fetch_array()){  
-        echo "<option value='$row2[0]'>$row2[0] | $row2[1]</option>";
-        }
-      }else if (in_array($fk2,$typefk) && $fk2==$key) {
-        echo "<option disabled selected>--- Pilih Data ---</option>";
-        $sql2=$db->query($query3);
-        while($row2=$sql2->fetch_array()){  
-        echo "<option value='$row2[0]'>$row2[0] | $row2[1]</option>";
-        }
-      }
-        echo "</select>";
-      }else{
-      echo "<input type='text' class='form-control' name='$key'>";
-      }
-     
-    }
-    ?>    
-      
-      </div>
-      <div class="modal-footer">
-        <button class="btn btn-secondary" data-dismiss="modal">Batal</button>
-        <button type="submit" class="btn btn-primary">Simpan</button>
-      </div>
-      </form>
-    </div>
-  </div>
-</div>
-
-<?php 
-      $sql=$db->query($query);
-      while($row=$sql->fetch_object()) {
+<?php
+//form
+$queryinfo=$db->query("select column_name,column_key,data_type,extra from information_schema.columns where table_name='$tabel' && table_schema='$database' and extra!='auto_increment'");
+while ($row=$queryinfo->fetch_object()){
+    $form_tambah[]=$row->column_name;
+    $form_edit[]=$row->column_name;
+  }
 ?>
-<div class="modal fade" id="edit<?php echo $row->$pk;?>">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5>Edit <?php echo modify("$tabel");?></h5>
-        <button class="close" data-dismiss="modal" ></button>
-      </div>
-      <form action="konek.php?tabel=<?php echo $tabel;?>&query=edit&id=<?php echo $row->$pk?>" method="POST" enctype="multipart/form-data">
-      <div class="modal-body">
-       <?php
-    foreach ($tambahlabel as $key) {
-      $editkey=$row->$key;
-
-      echo "<label>".modify($key)."</label>";
-
-      if(in_array($key,$typedate)){
-      echo "<input type='date' class='form-control' name='$key' value='$editkey'>";
-      }else if(in_array($key,$typefile)){
-      echo "<input type='file' class='form-control' name='$key' value='$editkey'>";
-      }else if(@in_array($key,$typefk)){
-        echo "<select class='form-control' name='$key'>";
-        if (in_array($fk,$typefk)  && $fk==$key) {
-        //echo "<option disabled selected>--- Pilih Data ---</option>";
-        $sql2=$db->query($query2);
-        while($row2=$sql2->fetch_array()){  
-          if($row2[0]==$editkey){
-        echo "<option value='$row2[0]' selected>$row2[0] | $row2[1]</option>";
-          }else{
-        echo "<option value='$row2[0]'>$row2[0] | $row2[1]</option>";
-      }
-        }
-      }else  if (in_array($fk2,$typefk)  && $fk2==$key) {
-        //echo "<option disabled selected>--- Pilih Data ---</option>";
-        $sql2=$db->query($query3);
-        while($row2=$sql2->fetch_array()){  
-          if($row2[0]==$editkey){
-        echo "<option value='$row2[0]' selected>$row2[0] | $row2[1]</option>";
-          }else{
-        echo "<option value='$row2[0]'>$row2[0] | $row2[1]</option>";
-      }
-        }
-      }
-        echo "</select>";
-      }else{
-      echo "<input type='text' class='form-control' name='$key' value='$editkey'>";
-      }
-     
-    }
+<br>
+<div class="box">
+<div class="box-body table-responsive">
+  <table id="tb" class="table table-bordered table-striped">
+  <thead style="font: bold 15px serif;">
+    <th>No</th>
+    <?php
+    if(@isset($view)){
+      foreach (@$view as $vkolom2) {?>
+      <th><?php echo modify($vkolom2);?></th>
+      <?php }}
+      foreach ($kolom as $key) {
+      if(@in_array($key,$hidden_tb)){      
+      }else if(@in_array($key,$ganti)){        
+        $isi=array_search($key, $ganti);        
+        $visi=$isiganti[$isi];
+        ?>      
+      <th><?php  echo modify($visi);?></th>
+      <?php
+      }else{?>
+      <th><?php echo modify($key);?></th>
+    <?php }}
+    if(@$_GET['print']){}else{
     ?>    
-      
-      </div>
-      <div class="modal-footer">
-        <button class="btn btn-secondary" data-dismiss="modal">Batal</button>
-        <button type="submit" class="btn btn-primary">Simpan</button>
-      </div>
-      </form>
-    </div>
-  </div>
-</div>
-<?php }?>
+    <th>Aksi</th>
+    <?php }?>
+  </thead>
+  <tbody>
+  <?php    
+  if(@isset($qnew)){
+  $query=$qnew.$where;
+  }
+  //echo "$query";
+   $sql=$db->query($query);
+   while($row=$sql->fetch_object()) {  
+        $no++;
+        ?>
+    <tr>
+      <td width="50px"><?php echo $no;?></td>
+      <?php
+      if(@isset($view)){
+        foreach ($view as $vkolom2) {?>
+      <td><?php echo $row->$vkolom2;?></td>
+      <?php }}
+    foreach ($baris as $key) {
+      if(@in_array($key,$hidden_tb)){      
+      }else if(@in_array($key,$ganti)){        
+        $isi=array_search($key, $ganti);        
+        $visi=$isiganti[$isi];
+        ?>      
+      <td><?php  echo $row->$visi;?></td>
+      <?php
+      }else if(in_array($key,$typerp)){?>
+      <td><?php  echo php_to_rp($row->$key)?></td>
+      <?php
+      }else if(in_array($key,$typeimg)){?>
+      <td><img src='<?php echo $key."-".$tabel."/".$row->$key?>' height=50></td>
+      <?php }else{?>      
+      <td><?php  echo $row->$key;?></td>
+      <?php
+      }}
+      if(@$_GET['print']){}elseif(@$_GET['laporan']){?>
+    <td width="50px">
+      <?php if(in_array($tabel,$print_satuan)){ ?>
+      <button class="btn btn-xs btn-success" onclick="location.href='tabel/print_satuan.php?print=<?php echo $tabel;?>&id=<?php echo $row->$pk;?>'"><i class="fa fa-print"></i> Print</button>
+      <?php }?>
+</td>
+      <?php }else{
+    ?>  
+    <td width="250px">
+      <?php if(in_array($tabel,$print_satuan)){ ?>
+      <button class="btn btn-xs btn-success" onclick="location.href='tabel/print_satuan.php?print=<?php echo $tabel;?>&id=<?php echo $row->$pk;?>'"><i class="fa fa-print"></i> Print</button>
+      <?php }if(in_array($tabel,$detail)){ ?>
+      <button class="btn btn-xs btn-success" onclick="location.href='?tabel=<?php echo $tabel."_detail";?>&id=<?php echo $row->$pk;?>'"><i class="fa fa-eye"></i> Detail</button>
+      <?php }if(!in_array($tabel,$no_edit) and @$_GET['tabel']){ ?>
+          <button class="btn btn-xs btn-info btn-edit" id="<?php echo $row->$pk;?>" ><i class="fa fa-edit"></i> Edit</button>
+      <?php }if(!in_array($tabel,$no_del) and @$_GET['tabel']){?>
+          <button class="btn btn-xs btn-danger" onclick="if(confirm('Are you sure you want to delete your post?')){location.href='konek.php?tabel=<?php echo $tabel;?>&query=hapus&id=<?php echo $row->$pk;?>';return false;};"><i class="fa fa-trash"></i> Hapus</button>
+          <?php }?>
+          </td>   
+          <?php }?>
+    </tr>
+    <?php }?>
+  </tbody>
+</table>
+  
